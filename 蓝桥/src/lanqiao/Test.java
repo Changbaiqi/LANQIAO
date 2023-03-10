e14239cd9d91171e91e820279a7426a5a5989a4f
Changbaiqi <2084069833@qq.com>
2023-03-11 01:16:24 +0800

    更新2023/3/11

--- 蓝桥/src/lanqiao/Test.java
+++ 蓝桥/src/lanqiao/Test.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Test {
-
+	
 	public static void main(String[] args) {
 		  Scanner sc=new Scanner(System.in);
 		  int n=sc.nextInt();
